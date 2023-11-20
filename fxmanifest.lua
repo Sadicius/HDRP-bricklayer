d3560978dd22f8bc295852b5d155856761ef8554
Sadicius <124639760+Sadicius@users.noreply.github.com>
2023-11-20 09:06:36 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,28 @@ fx_version "adamant"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game "rdr3"
 
-description 'Thrasherrr | qbr-construction, Converted And Altered By: Danglr'
+description 'Thrasherrr | qbr-construction, Converted And Altered Danglr'
 
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@rsg-core/shared/locale.lua',
+    --'locales/es.lua',
+    --'locales/*.lua',
+    'config.lua',
+}
 client_scripts {
-	'config.lua',
 	'client/client.lua',
 }
 
 server_scripts {
 	'server/server.lua',
-	'config.lua'
 }
 
-dependency 'rsg-core' -- https://github.com/qbcore-redm-framework/qbr-core
+dependency {
+    'rsg-core',
+    'ox_lib',
+}
+
+lua54 'yes'
 
 lua54 'yes'
